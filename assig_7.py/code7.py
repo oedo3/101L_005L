26d3a13e9a9585bd0b39b0e7bf781c3b16c16460
osayedoohonba <ooenw7@umsystem.edu>
2021-10-25 19:00:19 -0500

    Update code7.py

--- assig_7.py/code7.py
+++ assig_7.py/code7.py
@@ -1,13 +1,52 @@
-def get_mpg():
+########################################################################
+## CS 101 Lab
+## Assignment 7
+## Name: Osay E
+## Email: ooenw7@umsystem.edu
+##
+## PROBLEM : Create write a program to read through a file containing 
+## information about fuel economy and output the results to a file above 
+## a threshold that the user gives
+##
+########################################################################
+def get_minimum_mpg():
     try:
-        mpg = int(input('Enter the minimum mpg ==> '))
-        if mpg <= 0:
-            print('Fuel economy given must be graeter than 0')
-            get_mpg()
-        if mpg >= 100:
-            print('Fuel economy given must be less than 100')
-            get_mpg()
+        minimum_mpg = float(input("Enter the minimum mpg ==> "))
+        if minimum_mpg < 0:
+            print('Fuel economy given must be greater than 0')
+        elif minimum_mpg > 100:
+            print('Fuel economy must be less than 100')
+        else:
+            return minimum_mpg
     except:
         print('You must enter a number for the fuel economy')
+
+def get_input_file():
+    user_file = input('Enter the name of the input vehicle file ==> ')
+    try:
+        with open(user_file,'r') as read_file:
+            return [[data.strip() for data in line.strip().split('\t')] for line in read_file.readlines()]
+
+    except:
+        print('Could not open file',user_file)
+def get_output_file(minimum_mpg,file_data):
+    
+    try:
+        user_file = input('Enter the name of the file to output to ==> ')
+        with open(user_file, 'w') as write_file:
+            for data in file_data:
+                try:
+                    if minimum_mpg >= float(data[7]):
+                        write_file.write('{0:<5}{1:<40}{2:<40}{3:>10}\n'.format(data[0], data[1], data[2], data[7]))
+                except:
+                    print('Could not convert value invalid for vehicle', data[0], data[1], data[2])
+        return
+    except:
+        print('There is an IO Error', user_file)
+if __name__ == "__main__":
+    minimum_mpg = get_minimum_mpg()
+    file_data = get_input_file()
+    get_output_file(minimum_mpg, file_data)
+    
 
     
